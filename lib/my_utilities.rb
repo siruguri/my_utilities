52760823f30a754d7f8666965c92a834c1d41cd4
Sameer Siruguri <siruguri@gmail.com>
2014-02-11 19:17:00 -0800

    removed dependency on getoptlong

--- lib/my_utilities.rb
+++ lib/my_utilities.rb
@@ -3,6 +3,7 @@ require "my_utilities/version"
 module MyUtilities
   # MyUtilities class variables
   @help_string=""
+  @used_single_options = []
 
   class << self
     attr_accessor :help_string
@@ -22,6 +23,46 @@ EOS
     exit -1
   end
 
+  def self.process_cli(opt_map)
+    return [] if Hash != opt_map.class or opt_map.empty?
+    return_array = []
+
+    opt_array = []
+    key_of = {}
+    opt_map.each do |k, v|
+      # If one of the keys is not a symbol, let's abort.
+      return [] unless k.is_a? Symbol
+
+
+      matches = /^\-(.)(:\-\-(.*)?)/.match v
+      single_opt = long_opt = nil
+
+      # We will generate options regardless of whether the input string is valid.
+      if !matches.nil?
+        single_opt = matches[1]
+        long_opt = matches[3]
+      end
+
+      gen_opt1, gen_opt2 = generate_option_strings
+      single_opt ||= gen_opt1
+      long_opt ||= gen_opt2
+      key_of["--#{long_opt}"]=k
+
+      opt_array << ["--#{long_opt}," "-#{single_opt}", GetoptLong::OPTIONAL_ARGUMENT]
+    }
+
+    opts = GetoptLong.new opt_array
+
+    opts.each do |opt, arg|
+      return_array[key_of[opt]]=arg
+    end
+
+    return_array
+   end
+end
+
+  end
+
   class Logger
     DEBUG=4
     INFO=3
@@ -35,6 +76,10 @@ EOS
 
     def debug(mesg)
       self.puts mesg, DEBUG
+    end
+
+    def fatal(mesg)
+      self.puts mesg, FATAL
     end
 
     def puts(mesg, level=DEBUG)
@@ -62,6 +107,25 @@ EOS
       end
     end
   end
+
+  private 
+  def self.generate_option_strings
+    a=generate_single_option
+    return [a, "#{a}_long_option"]
+  end
+
+  def self.generate_single_option
+    # This will fail terribly after 62 incorrectly specified options
+    ([a..z]+[A..Z]+[0..9]).each do |opt|
+      unless @used_single_options.include? opt
+        @used_single_options << opt
+        return opt
+      end
+    end
+
+    return 'a'
+  end
+
 end
 
 class Dir
